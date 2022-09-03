291302516c1669f734ba712cbad5cf93f94cb8d8
DeepLabc <1987728735@qq.com>
2022-09-03 20:28:57 +0800

    test1

---
+++ neck/pafpn.py
@@ -0,0 +1,9 @@
+def ttt():
+    """
+    >>> ttt()
+    Traceback (most recent call last):
+        ...
+    SyntaxError: cannot assign to generator expression
+    >>> ttt()
+    Traceback (most recent call last):
+    """
